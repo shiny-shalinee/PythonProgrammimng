a0723566e436dee3f300b6160f51bb719831158f
Shalinee <130631224+shiny-shalinee@users.noreply.github.com>
2023-06-24 01:20:28 +0530

    Digital Clock.py

--- Digital Clock.py
+++ Digital Clock.py
@@ -1,17 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 #import all the required libraries first
 import sys
 from tkinter import *
 #import time library to obtain current time
 import time
-
-
-# In[2]:
 
 
 #create a function timing and variable current_time
@@ -22,9 +13,6 @@ def timing():
     clock.config(text=current_time)
     #clock will change after every 200 microseconds
     clock.after(200,timing)
-
-
-# In[3]:
 
 
 #Create a variable that will store our tkinter window
@@ -38,10 +26,6 @@ clock.grid(row=2,column=2,pady=25,padx=100)
 timing()
  
 
-
-# In[4]:
-
-
 #create a variable for digital clock
 digital=Label(root,text="Digital Clock",font="times 24 bold")
 digital.grid(row=0,column=2)
@@ -50,13 +34,9 @@ nota=Label(root,text="hours        minutes        seconds",font="times 15 bold")
 nota.grid(row=3,column=2)
 
 
-# In[5]:
-
-
 root.mainloop()
 
 
-# In[ ]:
 
 
 
